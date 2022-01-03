d50acb04df8037fdc302b5d1284fdf032319ab1b
andradeviniicius <viniciusdandrade01@gmail.com>
2022-01-02 21:55:59 -0300

    Repeticoes pt2

--- Curso em Video/Módulo E Repeticoes/Exercicios/Resolucao/script.js
+++ Curso em Video/Módulo E Repeticoes/Exercicios/Resolucao/script.js
@@ -3,6 +3,7 @@
         let inicio = document.querySelector('input#txti')
         let fim = document.querySelector('input#txtf')
         let passo = document.querySelector('input#txtp')
+        var res = document.querySelector('div#res')
         
         if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
             window.alert('[ERRO] Confira os dados')
@@ -11,9 +12,10 @@
             let i1 = Number(inicio.value)
             let f1 = Number(fim.value)
             let p1 = Number(passo.value)
-        
-        let res = document.querySelector('div#res')
-        
+            if(i1 < f1) {
+                
+            }
+
         for(let i = i1; f1 >= i; i += p1) {
             
             res.innerHTML += ''
@@ -22,15 +24,3 @@
         }
     }
     }
-
-
-
-
-            /* resolucao
-function calcular() {
-    let ini = document.querySelector('input#txti') //essa variavel nao existe fora do bloco calcular devido a propriedade do elemento Let
-    let fim = document.querySelector('input#txtf')
-    let pas = document.querySelector('input#txtp')
-
-    if (ini.value.lenght == 0 || )
-} */
--- Curso em Video/Módulo E Repeticoes/Exercicios/Tentativa ex01/teste.js
+++
@@ -1,7 +0,0 @@
-    var i1 = 0
-    var f1 = 4
-    var p1 = 3
-    
-    for(var i = i1;f1 >= i; i+=p1) {
-        console.log(i)
-    }
