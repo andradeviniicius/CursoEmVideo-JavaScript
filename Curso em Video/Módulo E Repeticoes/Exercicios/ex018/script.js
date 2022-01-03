0f2ccf32bc846b5f799e71c7e6c36e289303fc0c
andradeviniicius <viniciusdandrade01@gmail.com>
2022-01-02 22:28:39 -0300

    Aula14 Exercicios finalizados

---
+++ Curso em Video/Módulo E Repeticoes/Exercicios/ex018/script.js
@@ -0,0 +1,16 @@
+function tabuada() {
+    let  num = document.getElementById('txtn')
+    let  tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um numero!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(i=1;i<=10;i++) {
+            let item = document.createElement('option')
+            item.text = `${n} * ${i} = ${n*i}`
+            tab.appendChild(item)
+        }
+    }
+}
